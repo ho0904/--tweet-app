af16cde82129497a29fb746a2410b52a3defd8ed
ho0904 <ib230005ho@setouchicollege.com>
2024-07-13 15:33:01 +0900

    “p109まで実装”

--- app/Http/Controllers/Tweet/IndexController.php
+++ app/Http/Controllers/Tweet/IndexController.php
@@ -3,21 +3,21 @@
 namespace App\Http\Controllers\Tweet;
 
 use App\Http\Controllers\Controller;
-use App\Models\Tweet;
+use App\Services\TweetService;
 use Illuminate\Http\Request;
+
 
 class IndexController extends Controller
 {
     /**
      * Handle the incoming request.
      */
-    public function __invoke(Request $request)
-    {
-        $tweets = Tweet::orderBy('created_at', 'DESC')->get();
-        // $tweets 
-       // = Tweet::all();
-     
-       return view('tweet.index')
-       ->with('tweets', $tweets);
+   
+        public function __invoke(Request $request, TweetService $tweetService)
+        {
+            $tweets = $tweetService->getTweets();
+            return view('tweet.index')
+                ->with('tweets', $tweets);
+        }
     }
-}
+
